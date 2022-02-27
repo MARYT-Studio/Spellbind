e5c5f7da744e9e1a5dbd20a23924602da36e998a
Isaac Thompson <risinginiris2017@gmail.com>
2022-02-27 23:26:43 +0800

    Base logic. Have a try.

--- src/main/java/world/maryt/spellbind/Spellbind.java
+++ src/main/java/world/maryt/spellbind/Spellbind.java
@@ -1,85 +1,85 @@
 package world.maryt.spellbind;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent.Finish;
+import net.minecraft.util.math.EntityRayTraceResult;
 
-// The value here should match an entry in the META-INF/mods.toml file
+import java.util.Optional;
+import java.util.function.Predicate;
+
 @Mod("spellbind")
 public class Spellbind {
 
-    // Directly reference a log4j logger.
+//    public static final String MOD_ID = "spellbind";
+//    public static final String MOD_NAME = "Spellbind";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Spellbind() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        final IEventBus forgeBus = MinecraftForge.EVENT_BUS;
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        modBus.addListener(this::setup);
+        forgeBus.addListener(this::action);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("*---*---*---*---*---*---*---*---*");
+        LOGGER.info("Spellbind Mod by RisingInIris2017");
+        LOGGER.info("Proudly Presented by MARYT Studio");
+        LOGGER.info("*---*---*---*---*---*---*---*---*");
+        LOGGER.info(" _______  _______  _______  _        _        ______  _________ _        ______  ".replace('A','\\'));
+        LOGGER.info("(  ____ A(  ____ )(  ____ A( A      ( A      (  ___ A A__   __/( (    /|(  __  A ".replace('A','\\'));
+        LOGGER.info("| (    A/| (    )|| (    A/| (      | (      | (   ) )   ) (   |  A  ( || (  A  )".replace('A','\\'));
+        LOGGER.info("| (_____ | (____)|| (__    | |      | |      | (__/ /    | |   |   A | || |   ) |".replace('A','\\'));
+        LOGGER.info("(_____  )|  _____)|  __)   | |      | |      |  __ (     | |   | (A A) || |   | |".replace('A','\\'));
+        LOGGER.info("      ) || (      | (      | |      | |      | (  A A    | |   | | A   || |   ) |".replace('A','\\'));
+        LOGGER.info("/A____) || )      | (____/A| (____/A| (____/A| )___) )___) (___| )  A  || (__/  )".replace('A','\\'));
+        LOGGER.info("A_______)|/       (_______/(_______/(_______/|/ A___/ A_______/|/    )_)(______/ ".replace('A','\\'));
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    private Optional<Object> action(Finish event) {
+        if(event.getEntity() instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) (event.getEntity());
+            // 这里检测实体的地方需要重写
+            if(checkTheTarget(player, 10).get() instanceof LivingEntity) {
+                LivingEntity target = (LivingEntity) (checkTheTarget(player, 10).get());
+                if(!player.world.isRemote) {
+                    player.sendMessage(ITextComponent.getTextComponentOrEmpty(target.getEntityString() + " are spotted"), null);
+                }
+            }
+        }
+        return null;
     }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("spellbind", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
-        });
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
+    
+    private Optional<Object> checkTheTarget(PlayerEntity player, int distance) {
+        Vector3d vector3d = player.getEyePosition(1.0F);
+        Vector3d vector3d1 = player.getLook(1.0F).scale((double)distance);
+        Vector3d vector3d2 = vector3d.add(vector3d1);
+        AxisAlignedBB axisalignedbb = player.getBoundingBox().expand(vector3d1).grow(1.0D);
+        int i = distance * distance;
+        Predicate<Entity> predicate = (p_217727_0_) -> {
+            return !p_217727_0_.isSpectator() && p_217727_0_.canBeCollidedWith();
+        };
+        EntityRayTraceResult entityraytraceresult = ProjectileHelper.rayTraceEntities(player, vector3d, vector3d2, axisalignedbb, predicate, (double)i);
+        if (entityraytraceresult == null) {
+            return Optional.empty();
+        }
+        else {
+            return vector3d.squareDistanceTo(entityraytraceresult.getHitVec()) > (double)i ? Optional.empty() : Optional.of(entityraytraceresult.getEntity());
         }
     }
 }
