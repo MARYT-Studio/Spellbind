79846b0ac0069563f89b2c1a8394c6a86db9cf7b
Isaac Thompson <risinginiris2017@gmail.com>
2022-02-28 10:23:55 +0800

    First action type: apply potion effect

--- src/main/java/world/maryt/spellbind/Spellbind.java
+++ src/main/java/world/maryt/spellbind/Spellbind.java
@@ -21,11 +21,13 @@ import net.minecraft.util.math.EntityRayTraceResult;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import world.maryt.spellbind.actions.ApplyPotionEffect;
+
 @Mod("spellbind")
 public class Spellbind {
 
 //    public static final String MOD_ID = "spellbind";
-//    public static final String MOD_NAME = "Spellbind";
+    public static final String MOD_NAME = "Spellbind";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Spellbind() {
@@ -33,12 +35,12 @@ public class Spellbind {
         final IEventBus forgeBus = MinecraftForge.EVENT_BUS;
 
         modBus.addListener(this::setup);
-        forgeBus.addListener(this::action);
+        forgeBus.addListener(this::spell);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("*---*---*---*---*---*---*---*---*");
-        LOGGER.info("Spellbind Mod by RisingInIris2017");
+        LOGGER.info(MOD_NAME + " Mod by RisingInIris2017");
         LOGGER.info("Proudly Presented by MARYT Studio");
         LOGGER.info("*---*---*---*---*---*---*---*---*");
         LOGGER.info(" _______  _______  _______  _        _        ______  _________ _        ______  ".replace('A','\\'));
@@ -51,35 +53,39 @@ public class Spellbind {
         LOGGER.info("A_______)|/       (_______/(_______/(_______/|/ A___/ A_______/|/    )_)(______/ ".replace('A','\\'));
     }
 
-    private Optional<Object> action(Finish event) {
-        if(event.getEntity() instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) (event.getEntity());
-            // 这里检测实体的地方需要重写
-            if(checkTheTarget(player, 10).get() instanceof LivingEntity) {
-                LivingEntity target = (LivingEntity) (checkTheTarget(player, 10).get());
-                if(!player.world.isRemote) {
-                    player.sendMessage(ITextComponent.getTextComponentOrEmpty(target.getEntityString() + " are spotted"), null);
+    private Optional<Object> spell(Finish event) {
+        if(event.getEntity() instanceof PlayerEntity) { // If a player fires LivingEntityUseItemEvent.Finish
+            PlayerEntity player = (PlayerEntity)(event.getEntity());
+            Optional<Object> target = checkTheTarget(player, 10.0); // Check if there is an entity spotted
+            if(target.isPresent()) {
+                Entity targetEntity = (Entity)(target.get());
+                if(targetEntity instanceof LivingEntity) {
+                    LivingEntity targetLivingEntity = (LivingEntity)(targetEntity);
+                    if(!player.world.isRemote) {
+                        boolean potionSuccess = new ApplyPotionEffect().applyPotionEffect(targetLivingEntity,1,1000,1);
+                        player.sendMessage(ITextComponent.getTextComponentOrEmpty(targetEntity.getEntityString() + " is spotted, has applied potion effect: " + potionSuccess), null);
+                    }
                 }
             }
         }
         return null;
     }
     
-    private Optional<Object> checkTheTarget(PlayerEntity player, int distance) {
+    private Optional<Object> checkTheTarget(PlayerEntity player, double distance) {
         Vector3d vector3d = player.getEyePosition(1.0F);
-        Vector3d vector3d1 = player.getLook(1.0F).scale((double)distance);
+        Vector3d vector3d1 = player.getLook(1.0F).scale(distance);
         Vector3d vector3d2 = vector3d.add(vector3d1);
         AxisAlignedBB axisalignedbb = player.getBoundingBox().expand(vector3d1).grow(1.0D);
-        int i = distance * distance;
+        double squareDistance = distance * distance;
         Predicate<Entity> predicate = (p_217727_0_) -> {
             return !p_217727_0_.isSpectator() && p_217727_0_.canBeCollidedWith();
         };
-        EntityRayTraceResult entityraytraceresult = ProjectileHelper.rayTraceEntities(player, vector3d, vector3d2, axisalignedbb, predicate, (double)i);
+        EntityRayTraceResult entityraytraceresult = ProjectileHelper.rayTraceEntities(player, vector3d, vector3d2, axisalignedbb, predicate, squareDistance);
         if (entityraytraceresult == null) {
             return Optional.empty();
         }
         else {
-            return vector3d.squareDistanceTo(entityraytraceresult.getHitVec()) > (double)i ? Optional.empty() : Optional.of(entityraytraceresult.getEntity());
+            return vector3d.squareDistanceTo(entityraytraceresult.getHitVec()) > squareDistance ? Optional.empty() : Optional.of(entityraytraceresult.getEntity());
         }
     }
 }
---
+++ src/main/java/world/maryt/spellbind/actions/ApplyPotionEffect.java
@@ -0,0 +1,16 @@
+package world.maryt.spellbind.actions;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.*;
+
+import javax.annotation.Nullable;
+
+public class ApplyPotionEffect {
+    public boolean applyPotionEffect(LivingEntity target, int potionID, int duration, int amplifier) {
+        if(Effect.get(potionID) == null) {return false;}
+        else {
+            Effect effect = Effect.get(potionID);
+            return target.addPotionEffect(new EffectInstance(effect, duration, amplifier));
+        }
+    }
+}
